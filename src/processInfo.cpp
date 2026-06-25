604f6ed84e4117250b18a9f104926fba2ad0bf95
Mikkel Kronborg <mkronborg7@gmail.com>
2026-06-25 20:03:56 +0200

    add to the constrcut what it need to do

--- src/processInfo.cpp
+++ src/processInfo.cpp
@@ -13,6 +13,12 @@ ProcessInfo::ProcessInfo(std::string pid, std::string path) {
 
   pid_ = pid;
   path_ = path;
+
+  // need to check what kid of project it is
+  // after check if it contains a formater
+  // if it does copy path and file name then change it to fx filename.temp
+  // add avore own
+  // check .gitignore if it have the formater inside if not add it
 }
 
 bool match_name(const std::string& pid, const std::string& target) {
